75513c9ef8dde8ec0d40f808e5f25587766757e0
Hidran Arias <hidran@gmail.com>
2024-03-11 16:29:47 +0100

    prepares AWD ELASTIC VARIABBLES

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,12 +12,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
+            /*
+            host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+            */
             useFactory: (configService: ConfigService) => ({
                 type: 'mysql',
-                host: configService.get<string>('DATABASE_HOST'),
-                port: configService.get<number>('DATABASE_PORT'),
-                username: configService.get<string>('DATABASE_USER'),
-                password: configService.get<string>('DATABASE_PASSWORD'),
+                host: configService.get<string>('RDS_HOSTNAME'),
+                port: configService.get<number>('RDS_PORT'),
+                username: configService.get<string>('RDS_USERNAME'),
+                password: configService.get<string>('RDS_PASSWORD'),
                 database: configService.get<string>('DATABASE_NAME'),
                 entities: [User],
                 synchronize: true,
